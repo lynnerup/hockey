2c5389a4b837f611c623daf3d2647704c95eaf12
Søren Lynnerup <soren@lynnerup.nu>
2025-01-04 21:41:09 +0100

    Removed meta file.

--- metalligaen-live-feed.user.js
+++ metalligaen-live-feed.user.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         Add remaining time to Metalligaen.dk live feed
+// @namespace    MetalligaenLive
+// @version      1.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://metalligaen.dk/live/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=metalligaen.dk
+// @grant        none
+// ==/UserScript==
+
 function addRemainingTime() {
   $('#nav-tabContent tbody tr td:first-of-type').each((index, obj) => {
     var text = $(obj).text();
