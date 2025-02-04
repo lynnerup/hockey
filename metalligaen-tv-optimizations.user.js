3d8169aeca738cbc8fe9964a4a911b1df7c588a1
laquist <laquist12@gmail.com>
2025-02-04 21:05:36 +0100

    Run code on "playerReady" event instead of using setTimeout etc.

--- metalligaen-tv-optimizations.user.js
+++ metalligaen-tv-optimizations.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Metalligaen.tv player optimizations
 // @namespace    MetalligaenLive
-// @version      2025-02-04
+// @version      2025-02-03
 // @description  Fixer lortensen
 // @author       You
 // @match        https://www.metalligaen.tv/*
@@ -21,24 +21,33 @@
     e.stopImmediatePropagation();
   });
 
-  window.addEventListener('ionRouteDidChange', function () {
-    console.log('ionRouteDidChange');
-    runCode();
+  // window.addEventListener('ionRouteDidChange', function () {
+  //   console.log('ionRouteDidChange');
+  //   runCode();
+  // });
+
+  window.addEventListener('playerReady', async function (e) {
+    console.log('playerReady', e.target.localName, e);
+
+    if (e.target.localName == 'app-game-video') {
+      runCode();
+    }
   });
 
-  // Prevent overflow in fullscreen - because mobile browsers auto decides to fit or stretch
-  const css = [
-    `.vjs-fullscreen #app-video_html5_api {
-      height: 100vh !important;
-    }`,
-  ];
-  loadCustomStyling(css);
-
-  const scripts = ['https://vjs.zencdn.net/8.16.1/video.min.js'];
-  loadScripts(scripts, 0, runCode);
-
   function runCode() {
-    // Test solution for now
+    // Prevent overflow in fullscreen - because mobile browsers auto decides to fit or stretch
+    const css = [
+      `.vjs-fullscreen #app-video_html5_api {
+        height: 100vh !important;
+      }`,
+    ];
+    loadCustomStyling(css);
+
+    const scripts = ['https://vjs.zencdn.net/8.16.1/video.min.js'];
+    loadScripts(scripts, 0, optimizePlayer);
+  }
+
+  function optimizePlayer() {
     if (!window.location.href.startsWith('https://www.metalligaen.tv/da/game/')) {
       console.log('This is not a game page - returning.');
       return;
@@ -46,62 +55,55 @@
       console.log('This is a game page!');
     }
 
-    // TODO: Maybe we can do this smarter and check when the video element is loaded instead of a timeout
-    // Maybe just make a function that spam-checks this: window.customElements.get('app-root-play-site')
+    const videoPlayer = videojs('app-video');
+    const nativeVideoPlayer = document.getElementById('app-video_html5_api');
 
-    setTimeout(() => {
-      const videoPlayer = videojs('app-video');
-      const nativeVideoPlayer = document.getElementById('app-video_html5_api');
+    // Debugging
+    window.myVideoJsPlayer = videoPlayer;
+    console.log('videoPlayer:', videoPlayer);
+    // console.log('navigator.userActivation.hasBeenActive:', navigator.userActivation.hasBeenActive);
 
-      // Debugging
-      window.myVideoJsPlayer = videoPlayer;
-      console.log('videoPlayer:', videoPlayer);
-      // console.log('navigator.userActivation.hasBeenActive:', navigator.userActivation.hasBeenActive);
+    // Check own global player settings
+    if (navigator.userActivation.hasBeenActive) {
+      // If we don't check hasBeenActive, the browser might pause the player instead
+      console.log('Setting muted based on localStorage setting');
+      videoPlayer.muted((localStorage.getItem('mtv-muted') ?? '') === 'true');
+    }
 
-      // Check own global player settings
-      if (navigator.userActivation.hasBeenActive) {
-        // If we don't check hasBeenActive, the browser might pause the player instead
-        console.log('Setting muted based on localStorage setting');
-        videoPlayer.muted((localStorage.getItem('mtv-muted') ?? '') === 'true');
-      }
+    console.log('Setting volume based on localStorage setting');
+    videoPlayer.volume(Number.parseFloat(localStorage.getItem('mtv-volume') ?? 1.0));
 
-      console.log('Setting volume based on localStorage setting');
-      videoPlayer.volume(Number.parseFloat(localStorage.getItem('mtv-volume') ?? 1.0));
+    if (!videoPlayer.muted() == false) {
+      // Hide big mute icon
+      console.log('Hiding big mute icon');
+      document.getElementById('mute-overlay').style.display = 'none';
+    }
 
-      if (!videoPlayer.muted() == false) {
-        // Hide big mute icon
-        console.log('Hiding big mute icon');
-        document.getElementById('mute-overlay').style.display = 'none';
-      }
+    // Change default volume to 100%
+    if (videoPlayer.volume() == 0.5) {
+      console.log('Changing default volume from 50% to 100%');
+      videoPlayer.volume(1);
+    }
 
-      // Change default volume to 100%
-      if (videoPlayer.volume() == 0.5) {
-        console.log('Changing default volume from 50% to 100%');
-        videoPlayer.volume(1);
-      }
+    if (videoPlayer.paused()) {
+      console.log('Videoplayer is paused on load. Trigger play-event');
+      videoPlayer.play();
+    }
 
-      if (videoPlayer.paused()) {
-        console.log('Videoplayer is paused on load. Trigger play-event');
-        videoPlayer.play();
-      }
+    // Save and reuse settings across players
+    nativeVideoPlayer.addEventListener('volumechange', function (e) {
+      console.log('volumechange event - saving to localStorage (muted, volume)', e.target.muted, e.target.volume);
+      localStorage.setItem('mtv-muted', e.target.muted);
+      localStorage.setItem('mtv-volume', e.target.volume);
+    });
 
-      // Save and reuse settings across players
-      nativeVideoPlayer.addEventListener('volumechange', function (e) {
-        console.log('volumechange event - saving to localStorage (muted, volume)', e.target.muted, e.target.volume);
-        localStorage.setItem('mtv-muted', e.target.muted);
-        localStorage.setItem('mtv-volume', e.target.volume);
-      });
-
-      // // Double click
-      // videojs('my-player', {
-      //   userActions: {
-      //     doubleClick: function () {
-      //       videoPlayer.currentTime(videoPlayer.currentTime() + 10);
-      //     }
-      //   }
-      // });
-
-      // Tampermonkey auto-update test..
-    }, 5000); // 2000
+    // // Double click
+    // videojs('my-player', {
+    //   userActions: {
+    //     doubleClick: function () {
+    //       videoPlayer.currentTime(videoPlayer.currentTime() + 10);
+    //     }
+    //   }
+    // });
   }
 })();
